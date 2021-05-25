13c02dcdc12476eaa27cb22b63a6fdf4d80d7e76
Sameed Pervaiz <pervaiz.8@osu.edu>
2021-05-25 04:18:35 -0400

    Average over all given realizations
    We added extra maps that allow us to parse data over the entire space of
    possible {chemical potentials} x {realizations} for a given temperature,
    and then reduce (average) over the realization subspaces. This greatly
    reduces the error if multiple realizations are available per data set,
    as the errors are summed in quadrature and divided by N.
    
    The implementation using a struct full of std::maps is very
    uncomfortable, as it feels like we waste a lot of time looking up values
    in each individual map. In the future we should think about rewriting
    the code to reverse the ordering of the struct and outermost maps.

--- parse.cpp
+++ parse.cpp
@@ -24,6 +24,42 @@
 
 // Local headers
 
+// TODO: consider map of structs, rather than struct of maps
+struct unaveraged_data
+{
+    // ( mu -> X ), where X is one of:
+    //     - mu (identity)
+    //     - <H> (w/ error)
+    //     - <mz^2> (w/ error)
+    //     - (r -> Cm(r)) (w/ error)
+
+    // For each temperature, we handle a set of data files for various (possibly
+    // repeating) chemical potentials. We'll combine the data from each file on
+    // the fly here, and perform averaging later.
+    std::map<double, std::vector<double>> energies;
+    std::map<double, std::vector<double>> d_energies;
+    std::map<double, std::vector<double>> moments;
+    std::map<double, std::vector<double>> d_moments;
+    std::map<double, std::map<double, std::vector<double>>> Cms;
+    std::map<double, std::map<double, std::vector<double>>> d_Cms;
+};
+
+struct averaged_data
+{
+    // It's 2:47 AM and I don't know what I'm writing anymore; there is probably
+    // a better way to structure this I will think of later
+    std::map<double, double> energy;
+    std::map<double, double> d_energy;
+    std::map<double, double> moment;
+    std::map<double, double> d_moment;
+    std::map<double, std::map<double, double>> Cm;
+    std::map<double, std::map<double, double>> d_Cm;
+};
+
+void parse_file(std::string in_fname, unaveraged_data &data);
+averaged_data average_data(unaveraged_data &data);
+void print_to_file(std::string out_fname, averaged_data &data);
+
 int main()
 {
     namespace fs = std::filesystem;
@@ -37,186 +73,245 @@ int main()
         std::cerr << "Parsing inverse temperature \u03B2 = " << inverse_temp << "\n";
         // Now, move to the specified temperature subdir so we can start parsing the
         // data files for various chemical potentials mu_i:
-        std::ofstream parsed_file("beta_" + inverse_temp + "_parsed.dat");
         fs::current_path(p);
+        unaveraged_data data;
         for (auto &p : fs::directory_iterator(fs::current_path()))
         {
             // For now, only examine files from the first realization (*r1.out),
             // and only files that contain the local moment correlations (r*.out)
+            // TODO: add support for multiple realizations, and average over them.
             auto str = p.path().filename().string();
-            if (str.at(0) == 'r' && str.substr(str.size()-6) == "r1.out")
+            if (str.at(0) == 'r')
             {
-                std::cerr << str << std::endl;
-                // Now, extract the chemical potential, I guess
-                std::stringstream sstream(str.substr(str.find("mu")+2));
-                double mu;
-                sstream >> mu;
-                std::cout << "Parsing chemical potential \u03BC = " << mu << std::endl;
-                std::ifstream data_file(str);
-                std::ofstream correl_r_file("cf_beta_" + inverse_temp + "_mu_" + std::to_string(mu) + ".dat");
-
-                // Now, parse the file. For now, let's only track the average site energy
-                // So skip everything up until that line
-                std::string line;
-                do
-                {
-                    std::getline(data_file, line);
-                }
-                while (line.find("Average Energy") == line.npos);
-                std::cout << line << std::endl;
-                // Now print the line with its uncertainty as a function of chemical
-                // potential
-                sstream = std::stringstream(line.substr(line.find("=")+1));
-                double avg_energy, energy_uc;
-                sstream >> avg_energy >> energy_uc;
-                // Sometimes, energies are **********. so just ignore them for now?
-                if (sstream.fail())
-                {
-                    std::cerr << "NaN energy found!\n";
-                    avg_energy = std::nan("");
-                    energy_uc = std::nan("");
-                    sstream.clear();
-                }
-
-                std::cerr << "Skipping non-moment correlation function data!\n";
-                do
-                {
-                    std::getline(data_file, line);
-                }
-                while (line.find("mi2x-mi2x correlation function") == line.npos);
-                // Multiple distinct triples may have the same hypotenuse length,
-                // even among pythagorean triples (which are all integers). We have
-                // to average over these, so we must store a vector of correlations
-                // and errors for each distance, and perform the (weighted) averaging
-                // later.
-                std::map<double, std::vector<double>> moment_correls, moment_correl_errs;
-                std::map<double, double> fin_moment_correls, fin_moment_correl_errs;
-
-                // Skip the header line and start reading the cf data into our map
-                std::getline(data_file, line);
-                while (line.find("local") == line.npos)
-                {
-                    std::cerr << "Moment line found: " << line << std::endl;
-                    double x, y, cf, cf_uc;
-                    sstream = std::stringstream(line);
-                    sstream >> x >> y >> cf >> cf_uc;
-                    double r = std::hypot(x, y);
-                    std::cerr << "Line corresponds to radius " << r << std::endl;
-                    // the entries are coordinate symmetric, so ignore the lower
-                    // triangle of the (x,y) matrix
-                    if (y > x)
-                    {
-                        // TODO: organize the logic better
-                        std::cerr << "y > x, so skipping:\n";
-                        std::getline(data_file, line);
-                        continue;
-                    }
-
-                    // First, construct the vector if empty
-                    if (moment_correls.find(r) == moment_correls.end())
-                    {
-                        // TODO: there *has* to be a better way to do this
-                        moment_correls[r] = std::vector<double>();
-                        moment_correl_errs[r] = std::vector<double>();
-                    }
-                    // Now, tack on our values at the given radius
-                    moment_correls.at(r).push_back(cf);
-                    moment_correl_errs.at(r).push_back(cf_uc);
-
-                    std::getline(data_file, line);
-                }
-
-                // Now, for each radius, we perform averaging of the corr. fun, and
-                // add the errors in quadrature
-
-                for (auto it1 = moment_correls.begin(), it2 = moment_correl_errs.begin();
-                        it1 != moment_correls.end();
-                        it1++, it2++)
-                {
-                    const auto r = it1->first;
-                    fin_moment_correls[r] = 0.;
-                    fin_moment_correl_errs[r] = 0.;
-                    for (auto i1 = it1->second.begin(), i2 = it2->second.begin();
-                            i1 != it1->second.end();
-                            i1++, i2++)
-                    {
-                        fin_moment_correls[r] += *i1;
-                        fin_moment_correl_errs[r] += (*i2) * (*i2);
-                    }
-                    fin_moment_correls[r] /= it1->second.size();
-                    fin_moment_correl_errs[r] = std::sqrt(fin_moment_correl_errs[r]);
-                }
-
-                /*
-                // Now, discard the (0,0) line, AKA the variance
-                std::getline(data_file, line);
-
-                // Now, the nearest-neighbor (0,1) line
-                std::getline(data_file, line);
-                sstream = std::stringstream(line);
-
-                double n_moment, n_moment_uc;
-                // X Y MOMENT MOMENT_UNCERTAINTY
-                sstream >> n_moment >> n_moment >> n_moment >> n_moment_uc;
-
-                // Now, the next-nearest-neighbor (1,1) line
-                do
-                {
-                    std::getline(data_file, line);
-                }
-                while (line.find("1   1") == line.npos);
-                sstream = std::stringstream(line);
-                double nn_moment, nn_moment_uc;
-                sstream >> nn_moment >> nn_moment >> nn_moment >> nn_moment_uc;
-                */
-
-                // Now, local moment.
-                do
-                {
-                    std::getline(data_file, line);
-                }
-                while (line.find("0 0 local moment zz=") == line.npos);
-
-                sstream = std::stringstream(line.substr(line.find("=")+1));
-                double local_moment, local_moment_uc;
-                sstream >> local_moment >> local_moment_uc;
-
-                // We care about the squared local moment, so we update the uncertainty
-                // (relative errors added in quadrature) and measured values
-
-                // For the uncertainty, recall the following:
-                // delta(a*b) = |a*b| sqrt( (delta(a)/a)^2 + (delta(b)/b)^2 )
-                // In the case that a=b, this simplifies to
-                // delta(a^2) = |a^2| sqrt( 2 (delta(a)/a)^2 )
-                //            = |a^2| |delta(a)/a| sqrt(2)
-                //            = |a * delta(a)| sqrt(2)
-                local_moment_uc = std::abs(local_moment * local_moment_uc) * std::sqrt(2.);
-                local_moment *= local_moment;
-
-                // Now, print to files
-                parsed_file << "# mu   <H>    delta_<H>    <mz^2>    delta_<mz^2>    ";
-                for (auto i = fin_moment_correls.begin(); i != fin_moment_correls.end(); i++)
-                {
-                    auto r = i->first;
-                    parsed_file << "Cm(" << std::setprecision(3) << r <<
-                        ")    dCm(" << std::setprecision(3) << r << ")    ";
-                }
-                parsed_file << std::endl;
-
-                parsed_file << mu << " " << avg_energy << " " << energy_uc << " "
-                    << local_moment << " " << local_moment_uc << " ";
-
-                for (auto i1 = fin_moment_correls.begin(), i2 = fin_moment_correl_errs.begin();
-                        i1 != fin_moment_correls.end();
-                        i1++, i2++)
-                {
-                    parsed_file << std::setprecision(8) << i1->second - local_moment << " "
-                        << std::sqrt(i2->second * i2->second + local_moment_uc * local_moment_uc) << " ";
-                }
-                parsed_file << std::endl;
+                parse_file(str, data);
             }
         }
+        auto final_temp_data = average_data(data);
         fs::current_path(fs::current_path().parent_path().parent_path());
+        print_to_file("beta_" + inverse_temp + "_parsed.dat", final_temp_data);
     }
     return 0;
+}
+
+void parse_file(std::string in_fname, unaveraged_data &data)
+{
+    // Now, extract the chemical potential, I guess
+    std::stringstream sstream(in_fname.substr(in_fname.find("mu")+2));
+    double mu;
+    sstream >> mu;
+    std::cerr << "Parsing chemical potential \u03BC = " << mu << std::endl;
+
+    std::ifstream data_file(in_fname);
+
+    // Now, parse the file. For now, let's only track the average site energy
+    // So skip everything up until that line
+    std::string line;
+    do
+    {
+        std::getline(data_file, line);
+    }
+    while (line.find("Average Energy") == line.npos);
+    std::cerr << line << std::endl;
+
+    // Now print the line with its uncertainty as a function of chemical
+    // potential
+    sstream = std::stringstream(line.substr(line.find("=")+1));
+    double avg_energy, energy_uc;
+    sstream >> avg_energy >> energy_uc;
+    // Sometimes, energies are **********. so just ignore them for now?
+    if (sstream.fail())
+    {
+        std::cerr << "NaN energy found!\n";
+        avg_energy = std::nan("");
+        energy_uc = std::nan("");
+        sstream.clear();
+    }
+    data.energies[mu].push_back(avg_energy);
+    data.d_energies[mu].push_back(energy_uc);
+
+    std::cerr << "Skipping non-moment correlation function data!\n";
+    do
+    {
+        std::getline(data_file, line);
+    }
+    while (line.find("mi2x-mi2x correlation function") == line.npos);
+
+    // Multiple distinct triples may have the same hypotenuse length,
+    // even among pythagorean triples (which are all integers). We have
+    // to average over these, so we must store a vector of correlations
+    // and errors for each distance, and perform the (weighted) averaging
+    // later.
+    std::map<double, std::vector<double>> moment_correls, moment_correl_errs;
+    std::map<double, double> fin_moment_correls, fin_moment_correl_errs;
+
+    // Skip the header line and start reading the cf data into our map
+    std::getline(data_file, line);
+    while (line.find("local") == line.npos)
+    {
+        std::cerr << "Moment line found: " << line << std::endl;
+        double x, y, cf, cf_uc;
+        sstream = std::stringstream(line);
+        sstream >> x >> y >> cf >> cf_uc;
+        double r = std::hypot(x, y);
+        std::cerr << "Line corresponds to radius " << r << std::endl;
+        // the entries are coordinate symmetric, so ignore the lower
+        // triangle of the (x,y) matrix
+        if (y > x)
+        {
+            // TODO: organize the logic better
+            std::cerr << "y > x, so skipping:\n";
+            std::getline(data_file, line);
+            continue;
+        }
+
+        // Now, tack on our values at the given radius
+        data.Cms[mu][r].push_back(cf);
+        data.d_Cms[mu][r].push_back(cf_uc);
+
+        std::getline(data_file, line);
+    }
+
+    // Now, local moment.
+    do
+    {
+        std::getline(data_file, line);
+    }
+    while (line.find("0 0 local moment zz=") == line.npos);
+
+    sstream = std::stringstream(line.substr(line.find("=")+1));
+    double local_moment, local_moment_uc;
+    sstream >> local_moment >> local_moment_uc;
+
+    // We will square AFTER we average all the local moments over the number
+    // of realizations. This might be less numerically desirable than the original
+    // ordering, but let's revisit this later.
+
+    data.moments[mu].push_back(local_moment);
+    data.d_moments[mu].push_back(local_moment_uc);
+}
+
+averaged_data average_data(unaveraged_data &data)
+{
+    averaged_data final_data;
+
+    // Okay, so for a given temperature and chemical potential, we have basically
+    // turned a sequence of files with one entry per variable into a single file
+    // with a sequence of data entries per variable. Now we have to average over
+    // those files (in other words, over these new data entries).
+
+    // So first, iterate over these chemical potentials, since the chemical potential
+    // files are stored in the same directory (if they were in separate directories,
+    // our lives would be easier)
+
+    // Energy average
+    for (auto it1 = data.energies.begin(), it2 = data.d_energies.begin();
+            it1 != data.energies.end();
+            it1++, it2++)
+    {
+        const auto mu = it1->first;
+        final_data.energy[mu] = 0.;
+        final_data.d_energy[mu] = 0.;
+        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
+                jt1 != it1->second.end();
+                jt1++, jt2++)
+        {
+            final_data.energy[mu] += *jt1;
+            final_data.d_energy[mu] += (*jt2) * (*jt2);
+        }
+        final_data.energy[mu] /= it1->second.size();
+        final_data.d_energy[mu] = std::sqrt(final_data.d_energy[mu]) / it1->second.size();
+    }
+
+    // Moment average
+    for (auto it1 = data.moments.begin(), it2 = data.d_moments.begin();
+            it1 != data.moments.end();
+            it1++, it2++)
+    {
+        const auto mu = it1->first;
+        final_data.moment[mu] = 0.;
+        final_data.d_moment[mu] = 0.;
+        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
+                jt1 != it1->second.end();
+                jt1++, jt2++)
+        {
+            final_data.moment[mu] += *jt1;
+            final_data.d_moment[mu] += (*jt2) * (*jt2);
+        }
+        final_data.moment[mu] /= it1->second.size();
+        final_data.d_moment[mu] = std::sqrt(final_data.d_moment[mu]) / it1->second.size();
+
+        // We care about the squared local moment, so we update the uncertainty
+        // (relative errors added in quadrature) and measured values
+
+        // For the uncertainty, recall the following:
+        // delta(a*b) = |a*b| sqrt( (delta(a)/a)^2 + (delta(b)/b)^2 )
+        // In the case that a=b, this simplifies to
+        // delta(a^2) = |a^2| sqrt( 2 (delta(a)/a)^2 )
+        //            = |a^2| |delta(a)/a| sqrt(2)
+        //            = |a * delta(a)| sqrt(2)
+        final_data.d_moment[mu] = std::abs(final_data.moment[mu] * final_data.d_moment[mu]) * std::sqrt(2.);
+        final_data.moment[mu] *= final_data.moment[mu];
+    }
+
+    // Now, for each radius, we perform averaging of the corr. fun, and
+    // add the errors in quadrature
+
+    for (auto it1 = data.Cms.begin(), it2 = data.d_Cms.begin();
+            it1 != data.Cms.end();
+            it1++, it2++)
+    {
+        const auto mu = it1->first;
+        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
+                jt1 != it1->second.end();
+                jt1++, jt2++)
+        {
+            const auto r = jt1->first;
+            final_data.Cm[mu][r] = 0;
+            final_data.d_Cm[mu][r] = 0;
+            for (auto kt1 = jt1->second.begin(), kt2 = jt2->second.begin();
+                    kt1 != jt1->second.end();
+                    kt1++, kt2++)
+            {
+                final_data.Cm[mu][r] += *kt1;
+                final_data.d_Cm[mu][r] += (*kt2) * (*kt2);
+            }
+            final_data.Cm[mu][r] /= jt1->second.size();
+            final_data.d_Cm[mu][r] = std::sqrt(final_data.d_Cm[mu][r]) / jt1->second.size();
+        }
+    }
+
+    return final_data;
+}
+
+void print_to_file(std::string out_fname, averaged_data &data)
+{
+    std::ofstream output_file(out_fname);
+    // Now, print to files
+    output_file << "# mu   <H>    delta_<H>    <mz^2>    delta_<mz^2>    ";
+    // Take the first chemical potential, and print out all the lattice distances
+    // it doesn't matter which potential we use, since they're all on the same lattice.
+    for (auto it = data.Cm.begin()->second.begin(); it != data.Cm.begin()->second.end(); it++)
+    {
+        auto r = it->first;
+        output_file << "Cm(" << std::setprecision(3) << r <<
+            ")    dCm(" << std::setprecision(3) << r << ")    ";
+    }
+    output_file << std::endl;
+
+    // Now, loop over chemical potentials and print the data for each
+    for (auto it = data.Cm.begin(); it != data.Cm.end(); it++)
+    {
+        auto mu = it->first;
+        output_file << mu << " " << data.energy[mu] << " " << data.d_energy[mu] << " "
+            << data.moment[mu] << " " << data.d_moment[mu] << " ";
+
+        for (auto jt1 = data.Cm[mu].begin(), jt2 = data.d_Cm[mu].begin();
+                jt1 != data.Cm[mu].end();
+                jt1++, jt2++)
+        {
+            output_file << std::setprecision(8) << jt1->second - data.moment[mu] << " "
+                << std::sqrt(jt2->second * jt2->second + data.d_moment[mu] * data.d_moment[mu]) << " ";
+        }
+        output_file << std::endl;
+    }
 }
