53bd1cf6eb200cf34eca1fcbd0409b7ee8881dc6
Sameed Pervaiz <pervaiz.8@osu.edu>
2021-05-26 01:45:18 -0400

    Move chemical potential map outside the structs
    This refactors the data structs to now be wrapped inside a std::map
    sending chemical potentials to an image struct of variables, rather than
    each struct containing a map for each individual variable. This reduces
    the number of lookups we have to do to perform calculations for the
    averages and parsing, but should still preserve (or even improve)
    memory/cache performance, since the vectors we loop over are still
    contiguous within a given struct.

--- parse.cpp
+++ parse.cpp
@@ -17,6 +17,7 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <sstream>
 
 // Third-party Libraries
@@ -35,30 +36,34 @@ struct unaveraged_data
 
     // For each temperature, we handle a set of data files for various (possibly
     // repeating) chemical potentials. We'll combine the data from each file on
-    // the fly here, and perform averaging later.
-    std::map<double, std::vector<double>> energies;
-    std::map<double, std::vector<double>> d_energies;
-    std::map<double, std::vector<double>> moments;
-    std::map<double, std::vector<double>> d_moments;
-    std::map<double, std::map<double, std::vector<double>>> Cms;
-    std::map<double, std::map<double, std::vector<double>>> d_Cms;
+    // the fly here, and perform averaging later. Each instance of this struct
+    // stores a set of data corresponding to a distinct (beta, mu) pair.
+    std::vector<double> energies;
+    std::vector<double> d_energies;
+    std::vector<double> moments;
+    std::vector<double> d_moments;
+    std::map<double, std::vector<double>> Cms;
+    std::map<double, std::vector<double>> d_Cms;
 };
 
 struct averaged_data
 {
     // It's 2:47 AM and I don't know what I'm writing anymore; there is probably
     // a better way to structure this I will think of later
-    std::map<double, double> energy;
-    std::map<double, double> d_energy;
-    std::map<double, double> moment;
-    std::map<double, double> d_moment;
-    std::map<double, std::map<double, double>> Cm;
-    std::map<double, std::map<double, double>> d_Cm;
+    double energy;
+    double d_energy;
+    double moment;
+    double d_moment;
+    std::map<double, double> Cm;
+    std::map<double, double> d_Cm;
 };
 
-void parse_file(std::string in_fname, unaveraged_data &data);
-averaged_data average_data(unaveraged_data &data);
-void print_to_file(std::string out_fname, averaged_data &data);
+using unaveraged_map = std::map<double, unaveraged_data>;
+using averaged_map = std::map<double, averaged_data>;
+
+void parse_file(std::string in_fname, unaveraged_map &map);
+averaged_map average_data(unaveraged_map &map);
+void print_to_file(std::string out_fname, averaged_map &map);
 
 int main()
 {
@@ -74,7 +79,7 @@ int main()
         // Now, move to the specified temperature subdir so we can start parsing the
         // data files for various chemical potentials mu_i:
         fs::current_path(p);
-        unaveraged_data data;
+        unaveraged_map map;
         for (auto &p : fs::directory_iterator(fs::current_path()))
         {
             // For now, only examine files from the first realization (*r1.out),
@@ -83,17 +88,17 @@ int main()
             auto str = p.path().filename().string();
             if (str.at(0) == 'r')
             {
-                parse_file(str, data);
+                parse_file(str, map);
             }
         }
-        auto final_temp_data = average_data(data);
+        auto final_temp_map = average_data(map);
         fs::current_path(fs::current_path().parent_path().parent_path());
-        print_to_file("beta_" + inverse_temp + "_parsed.dat", final_temp_data);
+        print_to_file("beta_" + inverse_temp + "_parsed.dat", final_temp_map);
     }
     return 0;
 }
 
-void parse_file(std::string in_fname, unaveraged_data &data)
+void parse_file(std::string in_fname, unaveraged_map &map)
 {
     // Now, extract the chemical potential, I guess
     std::stringstream sstream(in_fname.substr(in_fname.find("mu")+2));
@@ -126,8 +131,8 @@ void parse_file(std::string in_fname, unaveraged_data &data)
         energy_uc = std::nan("");
         sstream.clear();
     }
-    data.energies[mu].push_back(avg_energy);
-    data.d_energies[mu].push_back(energy_uc);
+    map[mu].energies.push_back(avg_energy);
+    map[mu].d_energies.push_back(energy_uc);
 
     std::cerr << "Skipping non-moment correlation function data!\n";
     do
@@ -141,8 +146,6 @@ void parse_file(std::string in_fname, unaveraged_data &data)
     // to average over these, so we must store a vector of correlations
     // and errors for each distance, and perform the (weighted) averaging
     // later.
-    std::map<double, std::vector<double>> moment_correls, moment_correl_errs;
-    std::map<double, double> fin_moment_correls, fin_moment_correl_errs;
 
     // Skip the header line and start reading the cf data into our map
     std::getline(data_file, line);
@@ -165,8 +168,8 @@ void parse_file(std::string in_fname, unaveraged_data &data)
         }
 
         // Now, tack on our values at the given radius
-        data.Cms[mu][r].push_back(cf);
-        data.d_Cms[mu][r].push_back(cf_uc);
+        map[mu].Cms[r].push_back(cf);
+        map[mu].d_Cms[r].push_back(cf_uc);
 
         std::getline(data_file, line);
     }
@@ -186,13 +189,13 @@ void parse_file(std::string in_fname, unaveraged_data &data)
     // of realizations. This might be less numerically desirable than the original
     // ordering, but let's revisit this later.
 
-    data.moments[mu].push_back(local_moment);
-    data.d_moments[mu].push_back(local_moment_uc);
+    map[mu].moments.push_back(local_moment);
+    map[mu].d_moments.push_back(local_moment_uc);
 }
 
-averaged_data average_data(unaveraged_data &data)
+averaged_map average_data(unaveraged_map &map)
 {
-    averaged_data final_data;
+    averaged_map final_map;
 
     // Okay, so for a given temperature and chemical potential, we have basically
     // turned a sequence of files with one entry per variable into a single file
@@ -203,45 +206,33 @@ averaged_data average_data(unaveraged_data &data)
     // files are stored in the same directory (if they were in separate directories,
     // our lives would be easier)
 
-    // Energy average
-    for (auto it1 = data.energies.begin(), it2 = data.d_energies.begin();
-            it1 != data.energies.end();
-            it1++, it2++)
+    // Loop over chemical potential map
+    for (auto it = map.begin(); it != map.end(); it++)
     {
-        const auto mu = it1->first;
-        final_data.energy[mu] = 0.;
-        final_data.d_energy[mu] = 0.;
-        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
-                jt1 != it1->second.end();
-                jt1++, jt2++)
-        {
-            final_data.energy[mu] += *jt1;
-            final_data.d_energy[mu] += (*jt2) * (*jt2);
-        }
-        final_data.energy[mu] /= it1->second.size();
-        final_data.d_energy[mu] = std::sqrt(final_data.d_energy[mu]) / it1->second.size();
-    }
+        auto mu = it->first;
+        auto &data = it->second;
+        // Now, accumulate energies and moments, which have the same length.
+        // The energies and moments are simply the averages of the corresponding
+        // vectors, so we use std::accumulate to take advantage of parallelism (note:
+        // this might require explicitly detailing the execution policy)
 
-    // Moment average
-    for (auto it1 = data.moments.begin(), it2 = data.d_moments.begin();
-            it1 != data.moments.end();
-            it1++, it2++)
-    {
-        const auto mu = it1->first;
-        final_data.moment[mu] = 0.;
-        final_data.d_moment[mu] = 0.;
-        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
-                jt1 != it1->second.end();
-                jt1++, jt2++)
-        {
-            final_data.moment[mu] += *jt1;
-            final_data.d_moment[mu] += (*jt2) * (*jt2);
-        }
-        final_data.moment[mu] /= it1->second.size();
-        final_data.d_moment[mu] = std::sqrt(final_data.d_moment[mu]) / it1->second.size();
+        // NOTE: availability of std::reduce and std::transform_reduce is absolutely
+        // awful as of now (26-May-2021), so we'll stick to the serial versions
+        // for now...
 
-        // We care about the squared local moment, so we update the uncertainty
-        // (relative errors added in quadrature) and measured values
+        // For the errors, we add them in quadrature and then divide by N. We
+        // can accomplish this quickly by taking the inner product of the vector
+        // with itself, and then manipulating the result.
+        final_map[mu].energy = std::accumulate(data.energies.begin(), data.energies.end(), 0.) / data.energies.size();
+        final_map[mu].d_energy =
+            std::sqrt(std::inner_product(data.d_energies.begin(), data.d_energies.end(), data.d_energies.begin(), 0.)) / data.energies.size();
+
+        final_map[mu].moment = std::accumulate(data.moments.begin(), data.moments.end(), 0.) / data.moments.size();
+        final_map[mu].d_moment =
+            std::sqrt(std::inner_product(data.d_moments.begin(), data.d_moments.end(), data.d_moments.begin(), 0.)) / data.moments.size();
+
+        // So far we've only looked at the local moment, but we want the squared
+        // local moment, so we recalculate the value and its error
 
         // For the uncertainty, recall the following:
         // delta(a*b) = |a*b| sqrt( (delta(a)/a)^2 + (delta(b)/b)^2 )
@@ -249,68 +240,54 @@ averaged_data average_data(unaveraged_data &data)
         // delta(a^2) = |a^2| sqrt( 2 (delta(a)/a)^2 )
         //            = |a^2| |delta(a)/a| sqrt(2)
         //            = |a * delta(a)| sqrt(2)
-        final_data.d_moment[mu] = std::abs(final_data.moment[mu] * final_data.d_moment[mu]) * std::sqrt(2.);
-        final_data.moment[mu] *= final_data.moment[mu];
-    }
+        final_map[mu].d_moment = std::abs(final_map[mu].moment * final_map[mu].d_moment) * std::sqrt(2.);
+        final_map[mu].moment *= final_map[mu].moment;
 
-    // Now, for each radius, we perform averaging of the corr. fun, and
-    // add the errors in quadrature
 
-    for (auto it1 = data.Cms.begin(), it2 = data.d_Cms.begin();
-            it1 != data.Cms.end();
-            it1++, it2++)
-    {
-        const auto mu = it1->first;
-        for (auto jt1 = it1->second.begin(), jt2 = it2->second.begin();
-                jt1 != it1->second.end();
-                jt1++, jt2++)
+        // Now, for each radius, we perform averaging of the corr. fun, and
+        // add the errors in quadrature
+
+        for (auto jt = data.Cms.begin(); jt != data.Cms.end(); jt++)
         {
-            const auto r = jt1->first;
-            final_data.Cm[mu][r] = 0;
-            final_data.d_Cm[mu][r] = 0;
-            for (auto kt1 = jt1->second.begin(), kt2 = jt2->second.begin();
-                    kt1 != jt1->second.end();
-                    kt1++, kt2++)
-            {
-                final_data.Cm[mu][r] += *kt1;
-                final_data.d_Cm[mu][r] += (*kt2) * (*kt2);
-            }
-            final_data.Cm[mu][r] /= jt1->second.size();
-            final_data.d_Cm[mu][r] = std::sqrt(final_data.d_Cm[mu][r]) / jt1->second.size();
+            auto r = jt->first;
+            auto &cms = jt->second;
+            final_map[mu].Cm[r] = std::accumulate(cms.begin(), cms.end(), 0.) / cms.size();
+            final_map[mu].d_Cm[r] = std::sqrt(std::inner_product(data.d_Cms[r].begin(), data.d_Cms[r].end(), data.d_Cms[r].begin(), 0.)) / cms.size();
         }
     }
 
-    return final_data;
+    return final_map;
 }
 
-void print_to_file(std::string out_fname, averaged_data &data)
+void print_to_file(std::string out_fname, averaged_map &map)
 {
     std::ofstream output_file(out_fname);
     // Now, print to files
     output_file << "# mu   <H>    delta_<H>    <mz^2>    delta_<mz^2>    ";
     // Take the first chemical potential, and print out all the lattice distances
     // it doesn't matter which potential we use, since they're all on the same lattice.
-    for (auto it = data.Cm.begin()->second.begin(); it != data.Cm.begin()->second.end(); it++)
+    for (auto it = map.begin()->second.Cm.begin(); it != map.begin()->second.Cm.end(); it++)
     {
-        auto r = it->first;
+        const auto r = it->first;
         output_file << "Cm(" << std::setprecision(3) << r <<
             ")    dCm(" << std::setprecision(3) << r << ")    ";
     }
     output_file << std::endl;
 
     // Now, loop over chemical potentials and print the data for each
-    for (auto it = data.Cm.begin(); it != data.Cm.end(); it++)
+    for (auto it = map.begin(); it != map.end(); it++)
     {
-        auto mu = it->first;
-        output_file << mu << " " << data.energy[mu] << " " << data.d_energy[mu] << " "
-            << data.moment[mu] << " " << data.d_moment[mu] << " ";
+        const auto mu = it->first;
+        const auto &data = it->second;
+        output_file << mu << " " << data.energy << " " << data.d_energy << " "
+            << data.moment << " " << data.d_moment << " ";
 
-        for (auto jt1 = data.Cm[mu].begin(), jt2 = data.d_Cm[mu].begin();
-                jt1 != data.Cm[mu].end();
+        for (auto jt1 = data.Cm.begin(), jt2 = data.d_Cm.begin();
+                jt1 != data.Cm.end();
                 jt1++, jt2++)
         {
-            output_file << std::setprecision(8) << jt1->second - data.moment[mu] << " "
-                << std::sqrt(jt2->second * jt2->second + data.d_moment[mu] * data.d_moment[mu]) << " ";
+            output_file << std::setprecision(8) << jt1->second - data.moment << " "
+                << std::sqrt(jt2->second * jt2->second + data.d_moment * data.d_moment) << " ";
         }
         output_file << std::endl;
     }
